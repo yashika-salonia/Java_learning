d6e7f1bb34f80e5f18a513e3c37fc3c845d02099
yashika-salonia <yashikasalonia20@gmail.com>
2025-07-29 23:07:06 +0530

    better approach

--- src/dsa/array/setMatrixZero.java
+++ src/dsa/array/setMatrixZero.java
@@ -2,9 +2,9 @@ public class setMatrixZero {
 
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 2, 0, 4},
-            {5, 0, 7, 8},
-            {0, 10, 11, 12},
+            {0, 2, 9, 7},
+            {5, 1, 7, 8},
+            {0, 2, 0, 1},
         };
 
         // int[][] matrix={{-1},{2},{3}};
@@ -13,22 +13,28 @@ public class setMatrixZero {
 
     // works for non negative matrix only
     public static void setZeroes(int[][] matrix) {
-        int m=matrix.length;
         // System.out.println("m: "+m);
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
         for(int i=0; i<m; i++){
-            int n=matrix[i].length;
             // System.out.println("n: "+n);
             for(int j=0; j<n; j++){
+
                 if(matrix[i][j]==0){
-                    markRow(n, i, matrix);
-                    markCol(m, j, matrix);
+                    rows[i]=1;
+                    cols[j]=1;
+                    // markRow(n, i, matrix);
+                    // markCol(m, j, matrix);
                 }
             }
         }
         for(int i=0; i<m; i++){
-            int n=matrix[i].length;
             for(int j=0; j<n; j++){
-                if(matrix[i][j]==-1){
+                if(rows[i]==1 || cols[j]==1){
                     matrix[i][j]=0;
                 }
                 System.out.print(matrix[i][j]);
@@ -36,18 +42,18 @@ public class setMatrixZero {
             System.out.println();
         }
     }
-    static void markRow(int n,int i, int[][] matrix){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
-            }
-        }
-    }
-    static void markCol(int m,int j, int[][] matrix){
-        for(int i=0;i<m;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-1;
-            }
-        }
-    }
+    // static void markRow(int n,int i, int[][] matrix){
+    //     for(int j=0;j<n;j++){
+    //         if(matrix[i][j]!=0){
+    //             matrix[i][j]=-1;
+    //         }
+    //     }
+    // }
+    // static void markCol(int m,int j, int[][] matrix){
+    //     for(int i=0;i<m;i++){
+    //         if(matrix[i][j]!=0){
+    //             matrix[i][j]=-1;
+    //         }
+    //     }
+    // }
 }
