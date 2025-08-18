925e032d5c7e3d37ac7954cad36fe11620fbff73
yashika-salonia <yashikasalonia20@gmail.com>
2025-08-18 21:58:03 +0530

    optimal approach

--- src/dsa/array/stock_buy_sell.java
+++ src/dsa/array/stock_buy_sell.java
@@ -5,16 +5,30 @@ public class stock_buy_sell {
     }
 
     // brute force
-    static int maxProfit(int[] prices) {
-        int max=0;
+    // static int maxProfit(int[] prices) {
+    //     int max=0;
+    //     int n=prices.length;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //             if(prices[j]>prices[i]){
+    //                 max=Math.max(max,prices[j]-prices[i]);
+    //             }
+    //         }
+    //     }
+    //     return max;
+    // }
+    
+    // optimal approach
+    static int maxProfit(int[] prices){
+        int maxPf=0;
         int n=prices.length;
+        int minPrice=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(prices[j]>prices[i]){
-                    max=Math.max(max,prices[j]-prices[i]);
-                }
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
             }
+            maxPf=Math.max(maxPf,prices[i]-minPrice);
         }
-        return max;
-    } 
+        return maxPf;
+    }
 }
